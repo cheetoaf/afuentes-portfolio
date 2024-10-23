a24266d397a02d99f5404104639b3fe981ac1cdc
cheetoaf <cheetoaf@gmail.com>
2024-10-23 00:29:21 -0700

    built out backend>routes>user.js built out routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.js';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000; //User different port for the backend
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', userRoutes); // Mount user routes
 
 //Connect to MongoDB
 const connectToDatabase = async () => {
@@ -30,5 +32,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:${PORT');
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
