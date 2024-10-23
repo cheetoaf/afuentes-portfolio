605a99561540beaac743e099d5224a7589498aa4
cheetoaf <cheetoaf@gmail.com>
2024-10-23 00:10:32 -0700

    built out backend>routes>user.js built out routes

---
+++ backend/routes/user.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import User from '../models/User.js'; 
+const router = express.Router();
+
+// Create a new user
+router.post('/', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        const savedUser = await user.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+export default router;
