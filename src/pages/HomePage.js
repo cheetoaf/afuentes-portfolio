e7f44ff028c4fd5b599a1ea30b31b5ccd6334484
cheetoaf <cheetoaf@gmail.com>
2024-10-31 01:24:09 -0700

    Created images folder, created hiddenGame.js and hiddengame.css

---
+++ src/pages/HomePage.js
@@ -0,0 +1,20 @@
+//homePage
+import React from 'react';
+import './home.css';
+
+const homePage = () => {
+    return (
+        <div className='homepage'>
+            {/*Circle container*/}
+            <div className='profile-circle'>
+                {/*Profile Image*/}
+                <img src='../images/adam.png' alt='Adam Fuentes' className='profilePicture'/>
+            </div>
+
+
+
+        
+        </div>
+    )
+
+}
